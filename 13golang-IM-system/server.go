5aad90aa31e2473fdaa532cb05686f7fb457860b
wei <chetwei@163.com>
2022-05-17 14:22:57 +0800

    一个即时通讯服务器版本1

---
+++ 13golang-IM-system/server.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"sync"
+)
+
+type Server struct {
+	Ip   string
+	Port int
+	//在线用户的列表 key 是用户名  v是用户对象
+	OnlineMap map[string]*User
+	mapLock   sync.RWMutex //同步锁,用于协程同步操作
+
+	//消息广播的channel
+	Message chan string
+}
+
+//创建一个server的端口
+func NewServer(ip string, port int) *Server {
+	server := &Server{
+		Ip:        ip,                     //服务器ip
+		Port:      port,                   //服务器监听端口
+		OnlineMap: make(map[string]*User), //存放上线的用户
+		Message:   make(chan string),      //广播消息的管道
+	}
+	return server
+}
+
+//处理建立好的连接
+func (this *Server) Handeler(conn net.Conn) {
+	//根据连接的消息创建一个用户登录用户的信息
+	user := NewUser(conn, this)
+
+	user.Online()
+
+	//接受客户端发送的消息
+	go func() {
+		buf := make([]byte, 4096)
+		for {
+			n, err := conn.Read(buf)
+			if n == 0 {
+				user.Offline()
+				return
+			}
+			if err != nil && err != io.EOF {
+				fmt.Println("conn Read err:", err)
+				return
+			}
+			//提取用户的消息 去除换行符
+			msg := string(buf[:n-1])
+
+			//用户针对msg进行消息处理
+			user.DoMessage(msg)
+		}
+	}()
+
+	//当前handler 阻塞
+	select {}
+}
+
+//监听广播消息 Message通道，一旦有消息就发送给全部的在线User
+func (this *Server) ListenMessager() {
+	for {
+		msg := <-this.Message
+		//将msg发送给全部的在线user
+		this.mapLock.Lock()
+		for _, cli := range this.OnlineMap {
+			cli.C <- msg
+		}
+		this.mapLock.Unlock()
+	}
+}
+
+//广播消息 向 Message通道 写入消息
+func (this *Server) BroadCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+	this.Message <- sendMsg //向 Server的 通道发送消息
+}
+
+//启动服务器
+func (this *Server) Start() {
+	//socket listen
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", this.Ip, this.Port))
+
+	if err != nil {
+		fmt.Println("net.Listen err", err)
+		return
+	}
+	//close listen socket
+	defer listener.Close()
+
+	//启动监听Message的协程
+	go this.ListenMessager()
+
+	//循环等待建立连接
+	for {
+		//accept
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("listener accept err:", err)
+			continue
+		}
+		//使用协程 do handler
+		go this.Handeler(conn)
+	}
+
+}
---
+++ 13golang-IM-system/user.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"net"
+)
+
+type User struct {
+	Name string
+	Addr string
+	C    chan string //用户的消息通道
+	conn net.Conn
+
+	server *Server //当前的user是继承 Server的
+}
+
+//根据连接 创建一个用户的API
+func NewUser(conn net.Conn, server *Server) *User {
+	userAddr := conn.RemoteAddr().String()
+	user := &User{
+		Name:   userAddr,
+		Addr:   userAddr,
+		C:      make(chan string), //消息通道
+		conn:   conn,              //连接
+		server: server,
+	}
+	//启动监听当前 user channel消息的goroutine
+	go user.ListenMessage()
+
+	return user
+}
+
+//用户的上线业务
+func (this *User) Online() {
+
+	//用户上线了,将用户加入到online map中
+	this.server.mapLock.Lock()
+	this.server.OnlineMap[this.Name] = this //将上线的用户添加到map中
+	this.server.mapLock.Unlock()
+
+	//广播当前用户上线的消息
+	this.server.BroadCast(this, "已上线")
+}
+
+//用户的下线业务
+func (this *User) Offline() {
+	this.server.mapLock.Lock()
+	delete(this.server.OnlineMap, this.Name) //将上线的用户添加到map中
+	this.server.mapLock.Unlock()
+
+	//广播当前用户上线的消息
+	this.server.BroadCast(this, "下线")
+}
+
+//用户处理消息的业务
+func (this *User) DoMessage(msg string) {
+	this.server.BroadCast(this, msg)
+}
+
+//监听当前User channel 的方法，一旦有消息就直接发送给对端客户端
+func (this *User) ListenMessage() {
+	for {
+		msg := <-this.C                     //从当前用户的通道中取消息
+		this.conn.Write([]byte(msg + "\n")) //向连接端发送消息
+	}
+}
